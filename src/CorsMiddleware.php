dcc474e57ef285bb87eabb1bb8eb5ed2e335a812
Štěpán Zrník <stepan.zrnik@gmail.com>
2024-03-26 14:17:39 +0100

    if Access-Control-Allow-Credentials is set to true, Access-Control-Allow-Origin cannot contain *

--- src/CorsMiddleware.php
+++ src/CorsMiddleware.php
@@ -4,54 +4,63 @@ namespace Zrnik\SlimCors;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Slim\Middleware\BodyParsingMiddleware;
+use Slim\Psr7\Factory\ResponseFactory;
 
 /**
  * @see https://stackoverflow.com/a/9866124
  */
-class CorsMiddleware extends BodyParsingMiddleware
+class CorsMiddleware implements MiddlewareInterface
 {
+    /**
+     * @param string[] $allowedOrigins
+     * @param string[] $allowedMethods
+     */
     public function __construct(
         private readonly array $allowedOrigins = [],
         private readonly array $allowedMethods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     )
     {
-        parent::__construct();
     }
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $response = $handler->handle($request);
-
         $httpOrigin = $request->getServerParams()['HTTP_ORIGIN'] ?? null;
+
+        $response = match ($request->getMethod()) {
+            'OPTIONS' => (new ResponseFactory())->createResponse(),
+            default => $handler->handle($request),
+        };
 
         if ($httpOrigin !== null && in_array($httpOrigin, $this->allowedOrigins, true)) {
             $response = $response->withHeader('Access-Control-Allow-Origin', $httpOrigin);
             $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
-            $response = $response->withHeader('Access-Control-Max-Age', '86400');
-        } else if (count($this->allowedOrigins) === 0) {
-            $response = $response->withHeader('Access-Control-Allow-Origin', '*');
+            $response = $response->withHeader('Access-Control-Max-Age', '600');
+        } else if ($httpOrigin !== null && count($this->allowedOrigins) === 0) {
+            $response = $response->withHeader('Access-Control-Allow-Origin', $httpOrigin);
             $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
-            $response = $response->withHeader('Access-Control-Max-Age', '86400');
+            $response = $response->withHeader('Access-Control-Max-Age', '600');
         }
 
-        $httpAccessControlRequestMethod = $request->getServerParams()['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] ?? null;
+        if ($request->getMethod() === 'OPTIONS') {
+            $accessControlRequestMethods = $request->getHeader("Access-Control-Request-Method");
+            if (count($accessControlRequestMethods) > 0) {
+                $response = $response->withAddedHeader(
+                    'Access-Control-Allow-Methods',
+                    implode(', ', $this->allowedMethods)
+                );
+            }
 
-        if ($httpAccessControlRequestMethod !== null) {
-            $response = $response->withHeader(
-                'Access-Control-Allow-Methods',
-                implode(', ', $this->allowedMethods)
-            );
-        }
+            $accessControlRequestHeaders = $request->getHeader("Access-Control-Request-Headers");
+            $accessControlRequestHeaders = explode(',', implode(',', $accessControlRequestHeaders));
 
-        $httpAccessControlRequestHeaders = $request->getServerParams()['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'] ?? null;
-
-        if ($httpAccessControlRequestMethod !== null) {
-            $response = $response->withHeader(
-                'Access-Control-Allow-Headers',
-                $httpAccessControlRequestHeaders
-            );
+            if (count($accessControlRequestHeaders) > 0) {
+                $response = $response->withAddedHeader(
+                    'Access-Control-Allow-Headers',
+                    implode(', ', $accessControlRequestHeaders)
+                );
+            }
         }
 
         return $response;
